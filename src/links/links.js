4861c378f42e17f179eeac9a2e5384bbb3191802
ivanherve <ivanherve1411@gmail.com>
2020-10-22 08:48:15 +0200

    EmptyList Component

--- src/links/links.js
+++ src/links/links.js
@@ -1,5 +1,22 @@
+import backgroundImg from '../img/Shrug-Emoji.jpg';
+import React from 'react';
+
 const domain = 'localhost:8000/';
-export const apiUrl = `http://${domain}`;
+
+export let apiUrl = `http://${domain}`;
+
+export const checkUrl = (url) => {
+    var request = new XMLHttpRequest();
+    request.open('GET', apiUrl, true);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4) {
+            if (request.status !== 200) domain = 'localhost:8000/'
+            console.log(request.status, apiUrl)
+        }
+    };
+    request.send();
+}
+
 export const postRequest = {
     method: 'post',
 };
@@ -46,3 +63,45 @@ export const variantElement = (e, u) => {
 export const loadingTime = 2000;
 
 //export const isAdmin = JSON.parse(sessionStorage.getItem('userData')).user.Profil_Id === 3;
+
+export const styles = {
+    image: {
+        backgroundImage: 'url(' + backgroundImg + ')',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+    },
+    emptyList: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '50vh',
+        fontSize: '2rem',
+        fontWeight: 'bold',
+        color: '#00f',
+        backgroundColor: 'rgba(255, 255, 255,0.8)'
+    },
+    zoom: {
+        transform: 'scale(1.1)',
+        transition: 'transform .2s', /* Animation */
+        cursor: 'pointer',
+        height: '310px'
+    }
+}
+
+export const EmptyList = props => {
+    return (
+        <div style={styles.image}>
+            <div style={styles.emptyList}>
+                {props.msg}
+            </div>
+            <div style={{
+                fontStyle: 'italic', fontSize: '0.75rem', display: 'flex', flexDirection: 'column-reverse',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                Veuillez contacter votre administrateur
+            </div>
+        </div>
+    )
+}
